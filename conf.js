07c7b82330127954c8cf172c80aac66df12c26ab
petrmi <petr.micko@gmail.com>
2019-11-14 16:15:00 +0100

    config: remove unnecessary setup

--- conf.js
+++ conf.js
@@ -2,14 +2,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['./test_quotes.js'],
 
-    capabilities: {
-        browserName: 'chrome'
-    },
-
-    allScriptsTimeout: 600000,
-
     onPrepare: function () {
-        'use strict';
         browser.driver.manage().window().maximize();
     }
 };
