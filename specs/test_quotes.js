457da0259c6e6f8bd4d7367befa2cdf4c5dd88c2
petrmi <petr.micko@gmail.com>
2019-11-14 18:25:49 +0100

    rewrite example spec to ES6

--- specs/test_quotes.js
+++ specs/test_quotes.js
@@ -1,6 +1,7 @@
-var workflow_name = 'Automation Homework';
-var baseURL = 'https://qa-engineer.herokuapp.com', i, j;
-var quotesRequired = [  ['Awesome Quotes', 'Excellent time to become a missing person.'],
+const workflow_name = 'Automation Homework';
+const baseURL = 'https://qa-engineer.herokuapp.com';
+let i, j;
+const quotesRequired = [  ['Awesome Quotes', 'Excellent time to become a missing person.'],
                         ['Awesome Quotes', 'Beware of low-flying butterflies.'],
                         ['Awesome Quotes', 'I love deadlines. I love the whooshing sound they make as they fly by.'],
                         ['Awesome Quotes', 'Nothing so needs reforming as other people\'s habits.'],
@@ -11,30 +12,28 @@ var quotesRequired = [  ['Awesome Quotes', 'Excellent time to become a missing p
                         ['Famous Quotes', 'Yes there is a lot of people doing a great job out there.'],
                         ['Famous Quotes', 'If your life was a horse, you\'d have to shoot it.'] ];
 
-describe(workflow_name, function () {
+describe(workflow_name, () => {
 
-    it('Open browser with test page', function () {
+    it('Open browser with test page', () => {
         console.log('\nStart: ' + workflow_name);
         browser.driver.get(baseURL);
         browser.driver.sleep(2000);
         expect(browser.driver.getTitle()).toEqual('QA Engineer');
     });
 
-    it('Main Page: submit', function () {
+    it('Main Page: submit', () => {
         element(by.id('enter')).click();
         browser.driver.sleep(2000);
         expect(browser.getCurrentUrl()).toEqual(baseURL + '/code');
     });
 
-    it('Code Page: get secret code and submit like a robot', function () {
-        element(by.xpath("//input[@name='secret']")).getAttribute('value').then(function(secretCode){
+    it('Code Page: get secret code and submit like a robot', () => {
+        element(by.xpath("//input[@name='secret']")).getAttribute('value').then(secretCode => {
             element(by.xpath("//input[@name='code']")).sendKeys(secretCode);
         });
-        element(by.xpath("//input[@name='robot' and @type='checkbox']")).getWebElement().then(function(robotCheckBox){
-            robotCheckBox.isSelected().then(function(isSelected){
-                if (!isSelected){
-                    robotCheckBox.click();
-                }
+        element(by.xpath("//input[@name='robot' and @type='checkbox']")).getWebElement().then(robotCheckBox => {
+            robotCheckBox.isSelected().then(isSelected => {
+                if (!isSelected) robotCheckBox.click();
                 expect(element(by.xpath("//input[@name='robot' and @type='checkbox']")).isSelected()).toBeTruthy();
             });
         });
@@ -43,20 +42,20 @@ describe(workflow_name, function () {
         expect(browser.getCurrentUrl()).toEqual(baseURL + '/lists');
     });
 
-    it('Lists page: All categories are displayed', function(){
-        element(by.xpath("//strong[text()='Awesome Quotes' and parent::li[descendant::ul//li//span]]")).getWebElement().then(function(awesomeCategoryElement){
+    it('Lists page: All categories are displayed', () => {
+        element(by.xpath("//strong[text()='Awesome Quotes' and parent::li[descendant::ul//li//span]]")).getWebElement().then(awesomeCategoryElement => {
             expect(awesomeCategoryElement.isDisplayed()).toBeTruthy();
         });
-        element(by.xpath("//strong[text()='Famous Quotes' and parent::li[descendant::ul//li//span]]")).getWebElement().then(function(famousCategoryElement){
+        element(by.xpath("//strong[text()='Famous Quotes' and parent::li[descendant::ul//li//span]]")).getWebElement().then(famousCategoryElement => {
             expect(famousCategoryElement.isDisplayed()).toBeTruthy();
         });
     });
 
-    it('Lists page: All quotes are displayed within their categories, no extra quotes, no missing one', function(){
+    it('Lists page: All quotes are displayed within their categories, no extra quotes, no missing one', () => {
         quotesRequired.sort();
-        element.all(by.xpath("//span[not(contains(@class, 'score')) and ancestor::li[child::*[text()='Awesome Quotes']]]")).getText().then(function(awesomeQuotesFounded) {
+        element.all(by.xpath("//span[not(contains(@class, 'score')) and ancestor::li[child::*[text()='Awesome Quotes']]]")).getText().then(awesomeQuotesFounded => {
             awesomeQuotesFounded.sort();
-            element.all(by.xpath("//span[not(contains(@class, 'score')) and ancestor::li[child::*[text()='Famous Quotes']]]")).getText().then(function(famousQuotesFounded) {
+            element.all(by.xpath("//span[not(contains(@class, 'score')) and ancestor::li[child::*[text()='Famous Quotes']]]")).getText().then(famousQuotesFounded => {
                 famousQuotesFounded.sort();
                 for (i=0; i < quotesRequired.length; i++){
                     if (quotesRequired[i][0]==='Awesome Quotes'){
@@ -71,14 +70,14 @@ describe(workflow_name, function () {
         });
     });
 
-    it('Lists page: "Total score:" is the sum of all quote scores', function(){
-        element.all(by.xpath("//span[contains(@class,'score')]")).getText().then(function(scores){
-            var sumOfScores = 0;
-            scores.forEach(function(score){
+    it('Lists page: "Total score:" is the sum of all quote scores', () => {
+        element.all(by.xpath("//span[contains(@class,'score')]")).getText().then(scores => {
+            let sumOfScores = 0;
+            scores.forEach(score => {
                 sumOfScores += Number(score);
             });
-            element(by.xpath("//body")).getText().then(function(bodyText){
-                var pageTotalScore = Number(bodyText.split('Total score: ')[1]);
+            element(by.xpath("//body")).getText().then(bodyText => {
+                const pageTotalScore = Number(bodyText.split('Total score: ')[1]);
                 expect(pageTotalScore).toEqual(sumOfScores);
             });
         });
