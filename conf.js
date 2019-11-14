eeb6e8aaf5e24b15b04b2c293df4b928080149d9
petrmi <petr.micko@gmail.com>
2019-11-14 16:45:54 +0100

    setup console reporter

--- conf.js
+++ conf.js
@@ -1,8 +1,27 @@
+const consoleReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
+
     directConnect: true,
     specs: ['./test_quotes.js'],
+    jasmineNodeOpts: {print: () => {}}, // suppress default "dot" reporter
 
     onPrepare: function () {
+
         browser.driver.manage().window().maximize();
+
+        // setup console reporter
+        jasmine.getEnv().addReporter(
+            new consoleReporter({
+                spec: {
+                    displayDuration: true,
+                    displayStacktrace: true,
+                },
+                summary: {
+                    displayDuration: true,
+                    displayStacktrace: false,
+                },
+            })
+        );
     }
 };
