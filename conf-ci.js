603acc35d568f73eaab51ffc6f37ea67344ad502
petrmi <petr.micko@gmail.com>
2020-09-24 21:33:33 +0200

    config update for ci run

--- conf-ci.js
+++ conf-ci.js
@@ -1,17 +1,14 @@
 exports.config = {
 
-    directConnect: true,
     specs: ['./specs/test_quotes.js'],
     jasmineNodeOpts: {print: () => {}}, // suppress default "dot" reporter
     baseUrl: 'https://qa-homework.herokuapp.com',
     capabilities: {
         'browserName': 'chrome',
-        'args': ['--headless', '--disable-gpu', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage']
+        'args': ['--headless', '--disable-gpu', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage', "--window-size=1920,1080"]
     },
 
     onPrepare: function () {
-        browser.driver.manage().window().maximize();
-
         // for non-angular app - prevents protractor to wait for angular to be present when loading the test page
         browser.waitForAngularEnabled(false);
 
